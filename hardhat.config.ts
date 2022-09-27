0cd5ac5d0fd68fd16746001ed10834574b9cca74
Michał Mach <vipmachu@gmail.com>
2022-09-27 14:46:03 +0200

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,48 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-deploy';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      // outputSelection: {
+      //   '*': {
+      //     '*': ['storageLayout'],
+      //   },
+      // },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ETH_MAINNET_URL ?? '',
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    bob: {
+      default: 1,
+    },
+    alice: {
+      default: 2,
+    },
+    mat: {
+      default: 3,
+    },
+  },
+  mocha: {
+    timeout: 0,
+  },
+};
+
+export default config;

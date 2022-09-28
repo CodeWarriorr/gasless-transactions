eaf8fe56994f980421e1836115260ac2e92eed81
Michał Mach <vipmachu@gmail.com>
2022-09-28 10:23:30 +0200

    token paymaster deployment completion

--- deploy/6_token_paymaster.ts
+++ deploy/6_token_paymaster.ts
@@ -3,19 +3,19 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { ethers } from 'hardhat';
 import { fund } from '../utils/fund';
 
-const Uniswap3Router = '0xe592427a0aece92de3edee1f18e0157c05861564';
+// const Uniswap3Router = '0xe592427a0aece92de3edee1f18e0157c05861564';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy, get } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  // const Forwarder = await get('Forwarder');
-  // const RelayHub = await get('RelayHub');
+  const Forwarder = await get('Forwarder');
+  const RelayHub = await get('RelayHub');
 
   const TestUniswapDeployment = await get('TestUniswap');
 
-  const TokenPaymasterDeployment = await deploy('TokenPaymaster', {
+  await deploy('TokenPaymaster', {
     from: deployer,
     contract: 'TokenPaymaster',
     log: true,
@@ -24,15 +24,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     skipIfAlreadyDeployed: false,
   });
 
-  // const TokenPaymaster = await ethers.getContract('TokenPaymaster');
+  const TokenPaymaster = await ethers.getContract('TokenPaymaster');
 
-  // await TokenPaymaster.setTrustedForwarder(Forwarder.address);
-  // await TokenPaymaster.setRelayHub(RelayHub.address);
+  await TokenPaymaster.setTrustedForwarder(Forwarder.address);
+  await TokenPaymaster.setRelayHub(RelayHub.address);
 
-  // await fund(
-  //   TokenPaymasterDeployment.address,
-  //   ethers.utils.parseEther('1').toString()
-  // );
+  await fund(TokenPaymaster.address, ethers.utils.parseEther('1').toString());
 };
 
 export default func;

f66bc4acde8867f2ed8a66268855dcbc5a4ae305
Michał Mach <vipmachu@gmail.com>
2022-09-28 10:11:37 +0200

    accept everything paymaster tests

--- test/acceptEverythingPaymaster.test.ts
+++ test/acceptEverythingPaymaster.test.ts
@@ -10,47 +10,35 @@ import {
 } from '../typechain-types';
 import { getSigners } from '../utils/signers';
 
-const signSaveMessage = async (
+const domainSeparatorName = 'GSN Relayed Transaction';
+const domainVersion = '3';
+
+export const GsnRequestType = {
+  typeName: 'RelayRequest',
+  typeSuffix:
+    'RelayData relayData)RelayData(uint256 maxFeePerGas,uint256 maxPriorityFeePerGas,uint256 transactionCalldataGasUsed,address relayWorker,address paymaster,address forwarder,bytes paymasterData,uint256 clientId)',
+};
+
+interface MKRequest {
+  from: string;
+  to: string;
+  value: string;
+  gas: string;
+  nonce: string;
+  data: string;
+  validUntilTime: string;
+}
+
+const prepareRequest = async (
   forwarder: Forwarder,
   contract: MessageKeeper,
   wallet: SignerWithAddress,
   message: string
-) => {
-  const name = 'Forwarder';
-  const version = '1';
-
+): Promise<MKRequest> => {
   const [nonce, chainId] = await Promise.all([
     forwarder.getNonce(wallet.address),
     wallet.getChainId(),
   ]);
-
-  const domain = {
-    name,
-    version,
-    chainId,
-    verifyingContract: forwarder.address,
-  };
-
-  const EIP712DomainType = [
-    { name: 'name', type: 'string' },
-    { name: 'version', type: 'string' },
-    { name: 'chainId', type: 'uint256' },
-    { name: 'verifyingContract', type: 'address' },
-  ];
-
-  const ForwardRequestType = [
-    { name: 'from', type: 'address' },
-    { name: 'to', type: 'address' },
-    { name: 'value', type: 'uint256' },
-    { name: 'gas', type: 'uint256' },
-    { name: 'nonce', type: 'uint256' },
-    { name: 'data', type: 'bytes' },
-    { name: 'validUntilTime', type: 'uint256' },
-  ];
-
-  const requestTypes = {
-    ForwardRequest: ForwardRequestType,
-  };
 
   const transaction = await contract.populateTransaction.saveMessage(message);
 
@@ -64,9 +52,52 @@ const signSaveMessage = async (
     validUntilTime: ethers.constants.MaxUint256.toString(),
   };
 
-  const sig = await wallet._signTypedData(domain, requestTypes, req);
+  return req;
+};
 
-  return { sig, req };
+const signMessage = async (
+  req: RelayRequest,
+  forwarder: Forwarder,
+  wallet: SignerWithAddress
+) => {
+  const domain = {
+    name: domainSeparatorName,
+    version: domainVersion,
+    chainId: await wallet.getChainId(),
+    verifyingContract: forwarder.address,
+  };
+
+  const requestTypes = {
+    RelayData: [
+      { name: 'maxFeePerGas', type: 'uint256' },
+      { name: 'maxPriorityFeePerGas', type: 'uint256' },
+      { name: 'transactionCalldataGasUsed', type: 'uint256' },
+      { name: 'relayWorker', type: 'address' },
+      { name: 'paymaster', type: 'address' },
+      { name: 'forwarder', type: 'address' },
+      { name: 'paymasterData', type: 'bytes' },
+      { name: 'clientId', type: 'uint256' },
+    ],
+    RelayRequest: [
+      { name: 'from', type: 'address' },
+      { name: 'to', type: 'address' },
+      { name: 'value', type: 'uint256' },
+      { name: 'gas', type: 'uint256' },
+      { name: 'nonce', type: 'uint256' },
+      { name: 'data', type: 'bytes' },
+      { name: 'validUntilTime', type: 'uint256' },
+      { name: 'relayData', type: 'RelayData' },
+    ],
+  };
+
+  const reqToSign = {
+    ...req.request,
+    relayData: req.relayData,
+  };
+
+  const sig = await wallet._signTypedData(domain, requestTypes, reqToSign);
+
+  return sig;
 };
 
 describe('AcceptEverythingPaymaster', () => {
@@ -74,15 +105,9 @@ describe('AcceptEverythingPaymaster', () => {
   let RelayHub: RelayHub;
   let Forwarder: Forwarder;
   let AcceptEverythingPaymaster: AcceptEverythingPaymaster;
-  let deployer: SignerWithAddress;
   let alice: SignerWithAddress;
   let bob: SignerWithAddress;
 
-  /**
-   * TODO:
-   *
-   * Deploy relay hub
-   */
   beforeEach(async () => {
     await deployments.fixture('all');
     MessageKeeper = await ethers.getContract('MessageKeeper');
@@ -92,48 +117,24 @@ describe('AcceptEverythingPaymaster', () => {
       'AcceptEverythingPaymaster'
     );
 
-    ({ deployer, bob, alice } = await getSigners());
+    // Register OpenGsn Domain Separator
+    await Forwarder.registerDomainSeparator(domainSeparatorName, domainVersion);
+
+    // Register OpenGsn Request Type
+    await Forwarder.registerRequestType(
+      GsnRequestType.typeName,
+      GsnRequestType.typeSuffix
+    );
+
+    ({ bob, alice } = await getSigners());
   });
 
-  // /// @inheritdoc IRelayHub
-  // function relayCall(
-  //     string calldata domainSeparatorName,
-  //     uint256 maxAcceptanceBudget,
-  //     GsnTypes.RelayRequest calldata relayRequest,
-  //     bytes calldata signature,
-  //     bytes calldata approvalData
-  // )
-
   it('test', async () => {
-    const message = 'hello world from bob';
-    const { sig, req } = await signSaveMessage(
-      Forwarder,
-      MessageKeeper,
-      bob,
-      message
-    );
+    expect(await MessageKeeper.getMessage(bob.address)).to.eq('');
 
-    const abiCoder = new ethers.utils.AbiCoder();
-    const domainEncoded = abiCoder.encode(
-      ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
-      [
-        ethers.utils.keccak256(
-          ethers.utils.toUtf8Bytes(
-            'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'
-          )
-        ),
-        ethers.utils.keccak256(ethers.utils.toUtf8Bytes('Forwarder')),
-        ethers.utils.keccak256(ethers.utils.toUtf8Bytes('1')),
-        await bob.getChainId(),
-        Forwarder.address,
-      ]
-    );
-    const domainHash = ethers.utils.keccak256(domainEncoded);
-    const typeHash = ethers.utils.keccak256(
-      ethers.utils.toUtf8Bytes(
-        'ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data,uint256 validUntilTime)'
-      )
-    );
+    const message = 'hello world from bob';
+
+    const req = await prepareRequest(Forwarder, MessageKeeper, bob, message);
 
     const maxAcceptanceBudget = ethers.utils.parseEther('1');
     const RelayRequest: RelayRequest = {
@@ -147,21 +148,13 @@ describe('AcceptEverythingPaymaster', () => {
         forwarder: Forwarder.address,
         paymasterData: '0x',
         clientId: '0',
-        //   struct RelayData {
-        //     uint256 maxFeePerGas;
-        //     uint256 maxPriorityFeePerGas;
-        //     uint256 transactionCalldataGasUsed;
-        //     address relayWorker;
-        //     address paymaster;
-        //     address forwarder;
-        //     bytes paymasterData;
-        //     uint256 clientId;
-        // }
       },
     };
 
+    const sig = await signMessage(RelayRequest, Forwarder, bob);
+
     const tx = await RelayHub.connect(alice).relayCall(
-      domainHash,
+      domainSeparatorName,
       maxAcceptanceBudget,
       RelayRequest,
       sig,
@@ -169,21 +162,8 @@ describe('AcceptEverythingPaymaster', () => {
     );
 
     const receipt = await tx.wait(0);
+    // console.log('receipt.events', receipt.events);
 
-    console.log('receipt', receipt);
-    console.log('receipt.logs', receipt.logs);
-    console.log('receipt.events', receipt.events);
+    expect(await MessageKeeper.getMessage(bob.address)).to.eq(message);
   });
-
-  // it('saves message', async () => {
-  //   const message = 'hello world from bob';
-
-  //   expect(await MessageKeeper.getMessage(bob.address)).to.be.eq('');
-
-  //   await expect(MessageKeeper.connect(bob).saveMessage(message))
-  //     .to.emit(MessageKeeper, 'MessageSaved')
-  //     .withArgs(bob.address, message);
-
-  //   expect(await MessageKeeper.getMessage(bob.address)).to.be.eq(message);
-  // });
 });

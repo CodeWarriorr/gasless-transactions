8792132726c30e944cf166775e9284a4fb9f5e4a
Michał Mach <vipmachu@gmail.com>
2022-09-28 12:14:31 +0200

    fundTestUniswapToken

--- utils/fund.ts
+++ utils/fund.ts
@@ -1,3 +1,5 @@
+import { ethers } from 'hardhat';
+import { TestUniswap, TestToken } from '../typechain-types';
 import { getSigners } from './signers';
 
 export const fund = async (address: string, value: string) => {
@@ -7,4 +9,21 @@ export const fund = async (address: string, value: string) => {
     to: address,
     value: value,
   });
+};
+
+export const fundTestUniswapToken = async (
+  TestUniswap: TestUniswap,
+  address: string,
+  value: string
+) => {
+  const { deployer } = await getSigners();
+
+  const TestToken: TestToken = await ethers.getContractAt(
+    'contracts/paymasters/helpers/TestToken.sol:TestToken',
+    // '@opengsn/contracts/src/test/TestToken.sol:TestToken',
+    await TestUniswap.tokenAddress()
+  );
+
+  await TestToken.connect(deployer).mint(value);
+  await TestToken.connect(deployer).transfer(address, value);
 };

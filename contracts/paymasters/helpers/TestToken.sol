f2009c4f6889f200eb413a60f58c76cea6045c43
Michał Mach <vipmachu@gmail.com>
2022-09-28 12:11:56 +0200

    TestToken modified

--- contracts/paymasters/helpers/TestToken.sol
+++ contracts/paymasters/helpers/TestToken.sol
@@ -1,18 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC20, ERC20Permit} from './ERC20Permit.sol';
+import "hardhat/console.sol";
+
+import { ERC20, ERC20Permit } from "./ERC20Permit.sol";
 
 contract TestToken is ERC20Permit {
-  constructor(uint256 initialSupply) ERC20('Test Token', 'TOK') {
-    _mint(msg.sender, initialSupply);
-  }
+    constructor(uint256 initialSupply) ERC20("Test Token", "TOK") {
+        _mint(msg.sender, initialSupply);
+    }
 
-  function mint(address account, uint256 amount) public {
-    _mint(account, amount);
-  }
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
 
-  function burn(address account, uint256 amount) public {
-    _burn(account, amount);
-  }
+    function mintTo(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+        console.log("transferFrom spender", spender);
+        console.log("transferFrom from", from);
+        console.log("transferFrom amount", amount);
+        _spendAllowance(from, spender, amount);
+        console.log("transferFrom after _spendAllowance");
+        _transfer(from, to, amount);
+        console.log("transferFrom after _transfer");
+        return true;
+    }
 }

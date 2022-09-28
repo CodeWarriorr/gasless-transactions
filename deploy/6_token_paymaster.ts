e958fb180759eb90957f480a6d6eba2302b5aa4c
Michał Mach <vipmachu@gmail.com>
2022-09-28 12:14:03 +0200

    token paymaster deployment funding

--- deploy/6_token_paymaster.ts
+++ deploy/6_token_paymaster.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { ethers } from 'hardhat';
-import { fund } from '../utils/fund';
+import { TokenPaymaster } from '../typechain-types';
 
 // const Uniswap3Router = '0xe592427a0aece92de3edee1f18e0157c05861564';
 
@@ -10,8 +10,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, get } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const Forwarder = await get('Forwarder');
-  const RelayHub = await get('RelayHub');
+  const Forwarder = await ethers.getContract('Forwarder');
+  const RelayHub = await ethers.getContract('RelayHub');
 
   const TestUniswapDeployment = await get('TestUniswap');
 
@@ -24,12 +24,19 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     skipIfAlreadyDeployed: false,
   });
 
-  const TokenPaymaster = await ethers.getContract('TokenPaymaster');
+  const TokenPaymaster: TokenPaymaster = await ethers.getContract(
+    'TokenPaymaster'
+  );
 
   await TokenPaymaster.setTrustedForwarder(Forwarder.address);
   await TokenPaymaster.setRelayHub(RelayHub.address);
 
-  await fund(TokenPaymaster.address, ethers.utils.parseEther('1').toString());
+  // TokenPaymaster has no deposit for in receive()
+  // This is probably because eth refund goes straight to depositFor
+  // await fund(TokenPaymaster.address, ethers.utils.parseEther('1').toString());
+  await RelayHub.depositFor(TokenPaymaster.address, {
+    value: ethers.utils.parseEther('1'),
+  });
 };
 
 export default func;
